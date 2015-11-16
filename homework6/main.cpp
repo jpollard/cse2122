d9cb92cea0620ac7d6dfdc7d7a0ef3c4e9c4502c
Joseph Pollard <joseph.w.pollard+github@gmail.com>
2015-11-16 11:47:42 -0500

    Put the class website in the README file, and added some CDAccount tests to main.

--- homework6/main.cpp
+++ homework6/main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+bool transfer(BankAccount*, BankAccount*, double);
+
 int main(){
 
   BankAccount* mma = new MoneyMarketAccount();
@@ -13,6 +15,17 @@ int main(){
   if(mma->withdraw(300))
     cout << mma->getBalance() << endl;
   cout << cda->getBalance() << endl;
-  
+ 
+  transfer(mma, cda, 100);
+
+  cout << mma->getBalance() << " " << cda->getBalance() << endl;
+ 
   return 0;
+}
+
+bool transfer(BankAccount* from, BankAccount* to, double amount){
+  bool status = false;
+  status = from->withdraw(amount) && to->deposit(amount);
+
+  return status;
 }
